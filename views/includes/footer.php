f886133d7141a1f6b6faffd8e3099398da0ce3b3
sweetyams <willem@mookieyams.com>
2014-11-08 20:29:46 -0500

    Clean Foundation

---
+++ views/includes/footer.php
@@ -0,0 +1,13 @@
+    <script>
+      $(document).foundation();
+    </script>
+    
+    <!-- royalslider -->
+    <script src="/js/min/foundation-min.js"></script>
+    <script src="/js/min/custom.js"></script>
+
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
---
+++ views/includes/header.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>BV Catering</title>
+    
+    <!-- fonts -->
+    <link rel="stylesheet" type="text/css" href="css/fonts/brandontext.css">
+    <script src="//use.typekit.net/qyf7nhx.js"></script>
+    <script>try{Typekit.load();}catch(e){}</script>
+
+    <!-- stylesheets -->
+    <link rel="stylesheet" href="/css/foundation.css" />
+    <link rel="stylesheet" href="/css/custom.css" /> 
+
+    <!-- scripts and jquery -->
+    <script src="/js/min/jquery-min.js"></script>
+    <script src="/js/min/modernizr-min.js"></script>
+    
+  </head>
+  <body class="">
